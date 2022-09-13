a68e3882334d54925853b83c6c0d22ad7ce92c5f
Clever <79354627+zcc-fighting@users.noreply.github.com>
2022-09-13 20:59:55 +0800

    Add files via upload

---
+++ 第一次课堂作业/ExamSimulation/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace ExamSimulation
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkGray;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(182, 300);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始答题";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkGray;
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(385, 300);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 42);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "提交";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(217, 224);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 27);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "a";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(270, 224);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 27);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "+";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(325, 224);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 27);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "b";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(385, 224);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 27);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "=";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(445, 221);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 33);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(182, 90);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 27);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "总时长：";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(182, 140);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 27);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "60\'";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(362, 90);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(112, 27);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "单题时长：";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(362, 140);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(42, 27);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "15\'";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(545, 90);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(72, 27);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "得分：";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label10.Location = new System.Drawing.Point(545, 140);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(24, 27);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "0";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(800, 450);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label11.Location = new System.Drawing.Point(545, 315);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 27);
+            this.label11.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "考试界面";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label11;
+    }
+}
---
+++ 第一次课堂作业/ExamSimulation/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExamSimulation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int time1 = 60;
+        public int time2 = 15;
+        public int time3 = 2;
+        public bool isEnd = false;
+        public int a, b;
+        public char C;
+        public int score = 0;
+        //总时长倒计时
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            time1 = time1 - 1;
+            label6.Text = time1 + "'";
+            //结束处理
+            if (time1 == 0)
+            {
+                //停止计时
+                timer1.Stop();
+                timer2.Stop();
+                //恢复初始状态
+                isEnd = true;
+                button1.Text = "开始答题";
+                label1.Text = "a";
+                label3.Text = "b";
+                label6.Text = "60'";
+                label8.Text = "15'";
+                label10.Text = "0";
+                //给出结果
+                MessageBox.Show("考试结束!" +
+                    "你的得分为：" + score + "!");
+
+            }
+        }
+        //单题倒计时
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            time2 = time2 - 1;
+            label8.Text = time2 + "'";
+            //超时处理
+            if (time2 == 0)
+            {
+                //重新计时
+                time2 = 15;
+                timer2.Enabled = true;
+                timer2.Start();
+                time3 = 2;
+                timer3.Enabled = true;
+                timer3.Start();
+
+                //反馈
+                label11.Text = "超时！";
+                label11.ForeColor = Color.Red;
+                //出题
+                Random ra = new Random();
+                a = ra.Next(1, 10);
+                label1.Text = a.ToString();
+                char[] c = new char[2] { '+', '-' };
+                C = c[ra.Next(0, 2)];
+                label2.Text = C.ToString();
+                b = ra.Next(1, 10);
+                label3.Text = b.ToString();
+            }
+        }
+        //开始答题按钮
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //开始计时
+            time1 = 60;
+            timer1.Enabled = true;
+            timer1.Start();
+            time2 = 15;
+            timer2.Enabled = true;
+            timer2.Start();
+            isEnd = false;
+            score = 0;
+            label10.Text = score.ToString();
+            button1.Text = "重新答题";
+
+            //出题
+            Random ra = new Random();
+            a = ra.Next(1, 10);
+            label1.Text = a.ToString();
+            char[] c = new char[2] { '+', '-'};
+            C = c[ra.Next(0, 2)];
+            label2.Text = C.ToString();
+            b = ra.Next(1, 10);
+            label3.Text = b.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        //回答结果消息计时
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            time3 = time3 - 1;
+            if (time3 == 0) {
+                timer3.Stop();
+                label11.Text = "";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int yourResult;
+            int.TryParse(textBox1.Text, out yourResult);
+            if (isEnd == false)
+                switch (C)
+                {
+                    case '+':
+                        if (yourResult == a + b)
+                        {
+                            score += 5;
+                            label10.Text = score.ToString();
+                            label11.Text = "回答正确！";
+                            label11.ForeColor = Color.Green;
+                        }
+                        else
+                        {
+                            label11.Text = "回答错误！";
+                            label11.ForeColor = Color.Red;  
+                        }
+                        break;
+                    case '-':
+                        if (yourResult == a - b)
+                        {
+                            score += 5;
+                            label10.Text = score.ToString();
+                            label11.Text = "回答正确！";
+                            label11.ForeColor = Color.Green;
+
+                        }
+                        else
+                        {
+                            label11.Text = "回答错误！";
+                            label11.ForeColor = Color.Red;
+                        }
+                        break;
+                }
+            //重新计时
+            time2 = 15;
+            timer2.Enabled = true;
+            timer2.Start();
+            time3 = 2;
+            timer3.Enabled = true;
+            timer3.Start();
+            //出题
+            Random ra = new Random();
+            a = ra.Next(1, 10);
+            label1.Text = a.ToString();
+            char[] c = new char[2] { '+', '-' };
+            C = c[ra.Next(0, 2)];
+            label2.Text = C.ToString();
+            b = ra.Next(1, 10);
+            label3.Text = b.ToString();
+            //清空答案框
+            textBox1.Text = "";
+        }
+    }
+}
