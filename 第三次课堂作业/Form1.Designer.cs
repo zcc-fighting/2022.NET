c489b93b07c2585397f4e6428f3f12b28f01cbba
Clever <79354627+zcc-fighting@users.noreply.github.com>
2022-09-25 13:50:34 +0800

    Add files via upload

---
+++ 第三次课堂作业/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace FileManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.单词 = new System.Windows.Forms.ColumnHeader();
+            this.出现次数 = new System.Windows.Forms.ColumnHeader();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(163, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "选择源文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(163, 161);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "简单统计";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(163, 211);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "删去注释";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(163, 409);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 29);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "文件分析";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(287, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "原始行数：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(437, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "单词数：";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(377, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(522, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(287, 97);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "filename";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.单词,
+            this.出现次数});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(287, 211);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(253, 227);
+            this.listView1.TabIndex = 11;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // 单词
+            // 
+            this.单词.Tag = "单词";
+            this.单词.Text = "单词";
+            this.单词.Width = 133;
+            // 
+            // 出现次数
+            // 
+            this.出现次数.Tag = "出现次数";
+            this.出现次数.Text = "出现次数";
+            this.出现次数.Width = 120;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 497);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader 单词;
+        private System.Windows.Forms.ColumnHeader 出现次数;
+    }
+}
---
+++ 第三次课堂作业/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections;
+
+namespace FileManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        //记录文件路径
+        public string filePath;
+        //记录文件内容
+        public string fileString;
+
+        int lineNum = 0;//行数
+        int wordsNum = 0;//单词数
+
+        string[] words;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "请选择文件";
+            dialog.InitialDirectory = "E:\\C#work\\.NET架构设计仓库\\第二次课堂作业\\BankSystem";
+            dialog.Filter = "C#文件|*.cs|所有文件|*.*";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                filePath = dialog.FileName;
+                label5.Text = Path.GetFileName(dialog.FileName);
+            }
+        }
+        //简单统计
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (filePath != null)
+            {
+                StreamReader streamReader = new StreamReader(filePath);
+                string lineString = "";
+                //计算原始行数
+                while ((lineString = streamReader.ReadLine()) != null)
+                {
+                    if (lineString.Replace(" ","") != "")
+                    {
+                        lineNum++;
+                        //计算单词数
+                        //只考虑英文单词，所以数字和汉字都会被替换
+                        lineString = Regex.Replace(lineString, @"[^a-zA-Z\s]", " ");
+                        words = lineString.Split(' ');
+                        for (int i = 0; i < words.Length; i++)
+                        {
+                            if (words[i] != "")
+                            {
+                                wordsNum++;
+                            }
+                        }
+                    }
+                    
+                }
+                streamReader.Close();
+                label3.Text = lineNum.ToString();
+                label4.Text = wordsNum.ToString();
+                //重置记录，防止重复点击
+                lineNum = 0;
+                wordsNum = 0;
+            }
+            else 
+            {
+                MessageBox.Show("请先选择文件！");
+            }
+        }
+        //删除注释
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (filePath != null)
+            {
+                StreamReader streamReader = new StreamReader(filePath);
+                string lineString = "";
+                string[] lineStringArr = new string[1000]; 
+                //获取原始行数组
+                while ((lineString = streamReader.ReadLine()) != null)
+                {
+                    lineString= Regex.Replace(lineString, @"\s*//.*", "");
+                    if (lineString != "")
+                    {
+                        lineStringArr[lineNum]=lineString;
+                        lineNum++;
+                    }
+                    //计算单词数
+                    lineString = Regex.Replace(lineString, @"[^a-zA-Z\s]", " ");
+                    words = lineString.Split(' ');
+                    for (int i = 0; i < words.Length; i++)
+                    {
+                        if (words[i] != "")
+                        {
+                            wordsNum++;
+                        }
+                    }
+                }
+                streamReader.Close();
+
+                //写回文件
+                StreamWriter sw = new StreamWriter(filePath, false, Encoding.UTF8); 
+                for (int i = 0; i < lineStringArr.Length; i++)
+                {
+                    if (lineStringArr[i] != "")
+                    {
+                        sw.WriteLine(lineStringArr[i]);
+                    }
+                    sw.Flush();  // 清空缓存
+                }
+                sw.Close();  // 关闭文件
+               
+                //由于while循环退出时多加了一次，这里要减去
+                label3.Text = (lineNum-1).ToString();
+                label4.Text = wordsNum.ToString();
+                //重置记录，防止重复点击
+                lineNum = 0;
+                wordsNum = 0;
+
+            }
+            else 
+            {
+                MessageBox.Show("请先选择文件！");
+            }
+            
+        }
+
+        //文件分析
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (filePath != null)
+            {
+                //存储单词及其出现次数
+                Dictionary<string, int> wordList = new Dictionary<string, int>();
+                StreamReader streamReader = new StreamReader(filePath);
+                string lineString = "";
+                //统计各单词数量
+                while ((lineString = streamReader.ReadLine()) != null)
+                {
+                    if (lineString.Replace(" ", "") != "")
+                    {
+                        lineString = Regex.Replace(lineString, @"[^a-zA-Z\s]", " ");
+                        words = lineString.Split(' ');
+                        for (int i = 0; i < words.Length; i++)
+                        {
+                            if (words[i] != "" && !wordList.ContainsKey(words[i]))
+                            {
+                                wordList.Add(words[i], 1);
+                            }
+                            else if(words[i] != "" && wordList.ContainsKey(words[i]))
+                            {
+                                wordList[words[i]]++;
+                            }
+                        }
+                    }
+                }
+                streamReader.Close();
+
+                //显示单词列表
+                string[][] wordStringList = new string[wordList.Count][];
+                int index = 0;
+                foreach (var item in wordList)
+                {
+                    wordStringList[index] = new string[] { item.Key, item.Value.ToString() };
+                    index++;
+                }
+                for (int i = 0; i < wordList.Count; i++)
+                {
+                    ListViewItem item = new ListViewItem(wordStringList[i]);
+                    listView1.Items.Add(item);
+                }
+                this.listView1.EndUpdate();
+                //重置记录，防止重复点击
+                lineNum = 0;
+                wordsNum = 0;
+            }
+            else
+            {
+                MessageBox.Show("请先选择文件！");
+            }
+        }
+    }
+}
