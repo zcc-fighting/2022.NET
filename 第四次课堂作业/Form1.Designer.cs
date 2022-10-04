6f918055a6a7e01f9ce8386767fb90caee0c155f
Clever <79354627+zcc-fighting@users.noreply.github.com>
2022-10-04 21:02:25 +0800

    Add files via upload

---
+++ 第四次课堂作业/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Filebrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.菜单ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.重启程序ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.关闭程序ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.工具栏ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.复制文本内容ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.粘贴ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.删除ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 67);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(281, 371);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeExpand);
+            this.treeView1.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(299, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "filename";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(694, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(299, 67);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(489, 371);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "filetree";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.菜单ToolStripMenuItem,
+            this.工具栏ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(279, 28);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 菜单ToolStripMenuItem
+            // 
+            this.菜单ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.重启程序ToolStripMenuItem,
+            this.关闭程序ToolStripMenuItem});
+            this.菜单ToolStripMenuItem.Name = "菜单ToolStripMenuItem";
+            this.菜单ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.菜单ToolStripMenuItem.Text = "菜单";
+            // 
+            // 重启程序ToolStripMenuItem
+            // 
+            this.重启程序ToolStripMenuItem.Name = "重启程序ToolStripMenuItem";
+            this.重启程序ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.重启程序ToolStripMenuItem.Text = "更新数据";
+            this.重启程序ToolStripMenuItem.Click += new System.EventHandler(this.更新数据ToolStripMenuItem_Click);
+            // 
+            // 关闭程序ToolStripMenuItem
+            // 
+            this.关闭程序ToolStripMenuItem.Name = "关闭程序ToolStripMenuItem";
+            this.关闭程序ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.关闭程序ToolStripMenuItem.Text = "关闭程序";
+            this.关闭程序ToolStripMenuItem.Click += new System.EventHandler(this.关闭程序ToolStripMenuItem_Click);
+            // 
+            // 工具栏ToolStripMenuItem
+            // 
+            this.工具栏ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.复制文本内容ToolStripMenuItem,
+            this.粘贴ToolStripMenuItem,
+            this.删除ToolStripMenuItem});
+            this.工具栏ToolStripMenuItem.Name = "工具栏ToolStripMenuItem";
+            this.工具栏ToolStripMenuItem.Size = new System.Drawing.Size(68, 24);
+            this.工具栏ToolStripMenuItem.Text = "工具栏";
+            // 
+            // 复制文本内容ToolStripMenuItem
+            // 
+            this.复制文本内容ToolStripMenuItem.Name = "复制文本内容ToolStripMenuItem";
+            this.复制文本内容ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.复制文本内容ToolStripMenuItem.Text = "复制";
+            this.复制文本内容ToolStripMenuItem.Click += new System.EventHandler(this.复制文本内容ToolStripMenuItem_Click);
+            // 
+            // 粘贴ToolStripMenuItem
+            // 
+            this.粘贴ToolStripMenuItem.Name = "粘贴ToolStripMenuItem";
+            this.粘贴ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.粘贴ToolStripMenuItem.Text = "粘贴";
+            this.粘贴ToolStripMenuItem.Click += new System.EventHandler(this.粘贴ToolStripMenuItem_Click);
+            // 
+            // 删除ToolStripMenuItem
+            // 
+            this.删除ToolStripMenuItem.Name = "删除ToolStripMenuItem";
+            this.删除ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.删除ToolStripMenuItem.Text = "删除";
+            this.删除ToolStripMenuItem.Click += new System.EventHandler(this.删除ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 菜单ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 重启程序ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 关闭程序ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 工具栏ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 复制文本内容ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 粘贴ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 删除ToolStripMenuItem;
+    }
+}
---
+++ 第四次课堂作业/Form1.cs
@@ -0,0 +1,193 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Filebrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            load_directory();
+        }
+
+        //加载一、二级目录
+        private void load_directory()
+        {
+            //根节点
+            TreeNode rootNode = new TreeNode("此电脑");
+            rootNode.Name = "此电脑";
+            treeView1.Nodes.Add(rootNode);
+            //二级节点
+            add_panNode(rootNode);
+        }
+
+        private void add_panNode(TreeNode rootNode)
+        {
+            //循环遍历获取电脑的所有磁盘符
+            foreach (string drive in Environment.GetLogicalDrives())
+            {
+                //实例化DriveInfo对象
+                var dir = new DriveInfo(drive);
+                switch (dir.DriveType)           //判断驱动器类型
+                {
+                    case DriveType.Fixed:        //仅取固定磁盘盘符
+                        {
+                            //Split仅获取盘符字母
+                            TreeNode tNode = new TreeNode(dir.Name.Split(':')[0]);//节点视图的显示文本
+                            tNode.Name = dir.Name;//节点名称
+                            rootNode.Nodes.Add(tNode);//添加到根节点
+                            tNode.Nodes.Add("");//添加空节点以区分不同文件夹  
+                        }
+                        break;
+                }
+            }
+        }
+
+        //遍历父文件夹下面的所有文件夹和文件并添加到节点下
+        private void add_childNode(TreeNode parentNode)
+        {
+            DirectoryInfo directory = new DirectoryInfo(parentNode.Name);
+
+            if (parentNode.Name != "此电脑")//如果是打开"此电脑"节点就啥也不做
+            {
+                parentNode.Nodes.RemoveAt(0);//把空节点去掉
+                try//忽略无法访问的文件
+                {
+                    foreach (FileSystemInfo fsinfo in directory.GetFileSystemInfos())//获取当前文件夹中的所有文件夹与文件
+                    {
+                        //如果是文件就直接创建子节点加入父节点即可
+                        if (fsinfo is FileInfo)
+                        {
+                            TreeNode childNode = new TreeNode(fsinfo.Name);
+                            parentNode.Nodes.Add(childNode);
+                            childNode.Tag = "file";
+                        }
+                        //如果是文件夹除了创建子节点加入父节点,还要遍历其内容
+                        else if (fsinfo is DirectoryInfo)
+                        {
+                            TreeNode childNode = new TreeNode(fsinfo.Name);
+                            childNode.Name = fsinfo.FullName;
+                            parentNode.Nodes.Add(childNode);
+                            childNode.Nodes.Add("");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            add_childNode(e.Node);
+        }
+
+
+        string currentFilePath;
+        //读取文件并进行相应操作
+        private void treeView1_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            try 
+            {
+                if (e.Node.Tag.ToString() =="file")
+                {
+                    //从结点读取的路径开头为"此电脑\\E"等，需替换为"E:"才能被识别为盘符
+                    string filePath = e.Node.FullPath.Replace("此电脑\\C","C:").Replace("此电脑\\D", "D:").Replace("此电脑\\E", "E:");
+                    currentFilePath= filePath;//记录最近打开的文件
+                    string filename = System.IO.Path.GetFileName(e.Node.FullPath);//文件名  
+                    string extension = System.IO.Path.GetExtension(e.Node.FullPath);//扩展名 
+                    //根据扩展名对不同类型的文件执行相应的操作
+                    if(extension==".txt")
+                    {
+                        richTextBox1.Clear();
+                        label1.Text = filename;
+                        string[] lines = File.ReadAllLines(filePath);
+                        foreach (string line in lines)
+                        {
+                            richTextBox1.AppendText(line + Environment.NewLine);
+                        }
+                    }
+                    else if(extension==".exe")
+                    {
+                        Process process = new Process();
+                        process.StartInfo.FileName = filePath;
+                        process.Start();
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+        //清空文本框内容
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "filename";
+            richTextBox1.Clear();
+        }
+
+        private void 关闭程序ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        string currentText;
+        private void 复制文本内容ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            currentText = richTextBox1.Text;
+        }
+
+        private void 粘贴ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += currentText;
+            WriteForTxt(currentFilePath, currentText);
+        }
+        //向文件中写入字符串
+        private void WriteForTxt(string path, string currentText)
+        {
+            FileStream fs = new FileStream(path, FileMode.Append);
+            StreamWriter wr = null;
+            wr = new StreamWriter(fs);
+            wr.WriteLine(currentText);
+            wr.Close();
+        }
+        private void 删除ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FileInfo file = new FileInfo(currentFilePath);
+            file.Delete();
+        }
+
+        private void 更新数据ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Hide();   //先隐藏主窗体
+            Form1 form1 = new Form1(); //重新实例化此窗体
+            form1.ShowDialog();//已模式窗体的方法重新打开
+            this.Close();//原窗体关闭
+        }
+    }
+
+}
