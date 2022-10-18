00e7b7a4eaca6cf8a5d943e99bf54156a8cebdf5
Clever <79354627+zcc-fighting@users.noreply.github.com>
2022-10-19 00:03:44 +0800

    Add files via upload

---
+++ 第六次课堂作业/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.IO;
+
+namespace SchoolSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SQLiteHelper.CreateDB("E:\\dataTable.db");
+            SQLiteHelper.SetConnectionString("E:\\dataTable.db");
+            SQLiteHelper.InitDB();
+        }
+
+        //定义全局变量 单选按钮的文本
+        private string rbtStr = "";
+
+        //查询
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (rbtStr == "") 
+                {
+                    MessageBox.Show("请选择查找项！");
+                }
+                else
+                {
+                    string sql = "SELECT* FROM[" + rbtStr + "]";
+                    DataTable dt = SQLiteHelper.ExecuteQuery(sql);
+                    dataGridView1.DataSource = dt;
+                }
+
+                string str = System.DateTime.Now.ToString() + "  查找"+rbtStr;
+                SQLiteHelper.writeLog(str);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        //修改
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (radioButton1.Checked)
+                {
+                    string sql = "REPLACE INTO table1(ID, Name, clNum, headMaster) values (@ID, @Name, @clNum, @headMaster)";
+                    SQLiteParameter[] parameters =
+                    {
+                         new SQLiteParameter("@ID", textBox1.Text),
+                         new SQLiteParameter("@Name", textBox2.Text),
+                         new SQLiteParameter("@clNum", textBox3.Text),
+                         new SQLiteParameter("@headMaster", textBox4.Text)
+                    };
+                    SQLiteHelper.ExecuteNonQuery(sql, parameters);
+                }
+                else if (radioButton2.Checked)
+                {
+                    string sql = "REPLACE INTO table2(ID, Name, belong, headTeacher) values (@ID, @Name, @belong, @headTeacher)";
+                    SQLiteParameter[] parameters =
+                    {
+                         new SQLiteParameter("@ID", textBox1.Text),
+                         new SQLiteParameter("@Name", textBox2.Text),
+                         new SQLiteParameter("@belong", textBox3.Text),
+                         new SQLiteParameter("@headTeacher", textBox4.Text)
+                    };
+                    SQLiteHelper.ExecuteNonQuery(sql, parameters);
+                }
+                else if (radioButton3.Checked)
+                {
+                    string sql = "REPLACE INTO table3(ID, Name, belong, Tel) values (@ID, @Name, @Age, @Tel)";
+                    SQLiteParameter[] parameters =
+                    {
+                         new SQLiteParameter("@ID", textBox1.Text),
+                         new SQLiteParameter("@Name", textBox2.Text),
+                         new SQLiteParameter("@belong", textBox3.Text),
+                         new SQLiteParameter("@Tel", textBox4.Text)
+                    };
+                    SQLiteHelper.ExecuteNonQuery(sql, parameters);
+                }
+                else
+                    MessageBox.Show("请选择查询对象！");
+
+                string str = System.DateTime.Now.ToString() + "  " + rbtStr+"已修改";
+                SQLiteHelper.writeLog(str);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        //增加
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (radioButton1.Checked)
+                {
+                    string sql = "INSERT INTO table1(ID, Name, clNum, headMaster) values (@ID, @Name, @clNum, @headMaster)";
+                    SQLiteParameter[] parameters =
+                    {
+                         new SQLiteParameter("@ID", textBox1.Text),
+                         new SQLiteParameter("@Name", textBox2.Text),
+                         new SQLiteParameter("@clNum", textBox3.Text),
+                         new SQLiteParameter("@headMaster", textBox4.Text)
+                    };
+                    SQLiteHelper.ExecuteNonQuery(sql, parameters);
+                }
+                else if (radioButton2.Checked)
+                {
+                    string sql = "INSERT INTO table2(ID, Name, belong, headTeacher) values (@ID, @Name, @belong, @headTeacher)";
+                    SQLiteParameter[] parameters =
+                    {
+                         new SQLiteParameter("@ID", textBox1.Text),
+                         new SQLiteParameter("@Name", textBox2.Text),
+                         new SQLiteParameter("@belong", textBox3.Text),
+                         new SQLiteParameter("@headTeacher", textBox4.Text)
+                    };
+                    SQLiteHelper.ExecuteNonQuery(sql, parameters);
+                }
+                else if (radioButton3.Checked)
+                {
+                    string sql = "INSERT INTO table3(ID, Name, belong, Tel) values (@ID, @Name, @Age, @Tel)";
+                    SQLiteParameter[] parameters =
+                    {
+                         new SQLiteParameter("@ID", textBox1.Text),
+                         new SQLiteParameter("@Name", textBox2.Text),
+                         new SQLiteParameter("@belong", textBox3.Text),
+                         new SQLiteParameter("@Tel", textBox4.Text)
+                    };
+                    SQLiteHelper.ExecuteNonQuery(sql, parameters);
+                }
+                else
+                    MessageBox.Show("请选择查询对象！");
+                string str = System.DateTime.Now.ToString() + "  " + rbtStr+"插入数据";
+                SQLiteHelper.writeLog(str);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        //删除
+        private void button7_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (radioButton1.Checked)
+                {
+                    string sql = "DELETE FROM table1 WHERE ID = @ID";
+                    SQLiteParameter[] parameters =
+                    {
+                         new SQLiteParameter("@ID", textBox1.Text),
+                    };
+                    SQLiteHelper.ExecuteNonQuery(sql, parameters);
+                }
+                else if (radioButton2.Checked)
+                {
+                    string sql = "DELETE FROM table2 WHERE ID = @ID";
+                    SQLiteParameter[] parameters =
+                    {
+                         new SQLiteParameter("@ID", textBox1.Text),
+                    };
+                    SQLiteHelper.ExecuteNonQuery(sql, parameters);
+                    SQLiteHelper.ExecuteNonQuery(sql, parameters);
+                }
+                else if (radioButton3.Checked)
+                {
+                    string sql = "DELETE FROM table3 WHERE ID = @ID";
+                    SQLiteParameter[] parameters =
+                    {
+                         new SQLiteParameter("@ID", textBox1.Text),
+                    };
+                    SQLiteHelper.ExecuteNonQuery(sql, parameters);
+                    SQLiteHelper.ExecuteNonQuery(sql, parameters);
+                }
+                else
+                    MessageBox.Show("请选择查询对象！");
+
+                string str = System.DateTime.Now.ToString() + "  " + rbtStr + "删除数据";
+                SQLiteHelper.writeLog(str);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        //查看log表
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = @"E:\\LogFile.txt";
+                string LogTXT="";
+                string[] strArray = File.ReadAllLines(path);
+                for (int i = 0; i < strArray.Length; i++)
+                {
+                    LogTXT += strArray[i] + "\r\n";
+                }
+                MessageBox.Show(LogTXT);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        //重置数据库
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SQLiteHelper.dropTable();
+                SQLiteHelper.InitDB();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        //导入默认数据
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SQLiteHelper.inputData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            //还原事件源
+            RadioButton rbt = sender as RadioButton;
+            //判断是否被选中
+            if (rbt.Checked == true)
+            {
+                rbtStr = rbt.Tag.ToString();
+            }
+            label4.Text = "clNum";
+            label5.Text = "headMaster";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            //还原事件源
+            RadioButton rbt = sender as RadioButton;
+            //判断是否被选中
+            if (rbt.Checked == true)
+            {
+                rbtStr = rbt.Tag.ToString();
+            }
+            label4.Text = "belong";
+            label5.Text = "headTeacher";
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            //还原事件源
+            RadioButton rbt = sender as RadioButton;
+            //判断是否被选中
+            if (rbt.Checked == true)
+            {
+                rbtStr = rbt.Tag.ToString();
+            }
+            label4.Text = "belong";
+            label5.Text = "Tel";
+        }
+    }
+}
---
+++ 第六次课堂作业/SqliteHelper.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SQLite;
+using System.Data;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SchoolSystem
+{
+
+	
+	public class SQLiteHelper
+	{
+		private static string connectionString = string.Empty;
+
+		//根据数据源设置连接字符串。
+		public static void SetConnectionString(string datasource)
+		{
+			connectionString = string.Format("Data Source={0}", datasource);
+		}
+
+		//创建一个数据库文件。如果存在同名数据库文件，则会覆盖。
+		public static void CreateDB(string dbName)
+		{
+			if (!string.IsNullOrEmpty(dbName))
+			{
+				try 
+				{ 
+					SQLiteConnection.CreateFile(dbName);
+					//创建数据库时清空log表
+					System.IO.File.WriteAllText(@"E:\\LogFile.txt", string.Empty);
+					string str = System.DateTime.Now.ToString() + "  创建数据库 "+dbName+" 成功！";
+					writeLog(str);
+				}
+				catch (Exception) { throw; }
+			}
+		}
+
+		//写入记录
+		public static void writeLog(string str)
+		{
+			File.AppendAllText(@"E:\\LogFile.txt", str + Environment.NewLine);
+		}
+
+		public static void InitDB()
+		{
+			// 连接数据库    
+			SQLiteConnection connection = new SQLiteConnection(connectionString);
+			connection.Open();
+
+			//创建student表
+			string[] stColNames = new string[] { "ID", "Name", "belong", "Tel" };
+			string[] stColTypes = new string[] { "INTEGER PRIMARY KEY", "TEXT", "INTEGER", "INTEGER" };
+			string stTableName = "table3";
+			string stQueryString = "CREATE TABLE IF NOT EXISTS " + stTableName + "( " + stColNames[0] + " " + stColTypes[0];
+
+			for (int i = 1; i < stColNames.Length; i++)
+			{
+				stQueryString += ", " + stColNames[i] + " " + stColTypes[i];
+			}
+			stQueryString += "  ) ";
+			SQLiteCommand stDBCommand = connection.CreateCommand();
+			stDBCommand.CommandText = stQueryString;
+			stDBCommand.ExecuteNonQuery();
+
+			//创建class表
+			string[] clColNames = new string[] { "ID", "Name", "belong", "headTeacher" };
+			string[] clColTypes = new string[] { "INTEGER PRIMARY KEY", "TEXT", "INTEGER", "TEXT" };
+			string clTableName = "table2";
+			string clQueryString = "CREATE TABLE IF NOT EXISTS " + clTableName + "( " + clColNames[0] + " " + clColTypes[0];
+
+			for (int i = 1; i < clColNames.Length; i++)
+			{
+				clQueryString += ", " + clColNames[i] + " " + clColTypes[i];
+			}
+			clQueryString += "  ) ";
+			SQLiteCommand clDBCommand = connection.CreateCommand();
+			clDBCommand.CommandText = clQueryString;
+			clDBCommand.ExecuteNonQuery();
+
+			//创建school表
+			string[] scColNames = new string[] { "ID", "Name", "clNum", "headMaster" };
+			string[] scColTypes = new string[] { "INTEGER PRIMARY KEY", "TEXT", "INTEGER", "TEXT" };
+			string scTableName = "table1";
+			string scQueryString = "CREATE TABLE IF NOT EXISTS " + scTableName + "( " + scColNames[0] + " " + scColTypes[0];
+
+			for (int i = 1; i < scColNames.Length; i++)
+			{
+				scQueryString += ", " + scColNames[i] + " " + scColTypes[i];
+			}
+			scQueryString += "  ) ";
+			SQLiteCommand scDBCommand = connection.CreateCommand();
+			scDBCommand.CommandText = scQueryString;
+			scDBCommand.ExecuteNonQuery();
+
+			string str = System.DateTime.Now.ToString() + "  初始化数据库成功！";
+			writeLog(str);
+		}
+
+
+		//删除表
+		public static void dropTable()
+		{
+			// 连接数据库    
+			SQLiteConnection connection = new SQLiteConnection(connectionString);
+			if (connection.State != System.Data.ConnectionState.Open)
+			{
+				connection.Open();
+				SQLiteCommand command = new SQLiteCommand();
+				command.Connection = connection;
+				command.CommandText = "DROP TABLE IF EXISTS table1";
+				command.ExecuteNonQuery();//执行
+				command.CommandText = "DROP TABLE IF EXISTS table2";
+				command.ExecuteNonQuery();//执行
+				command.CommandText = "DROP TABLE IF EXISTS table3";
+				command.ExecuteNonQuery();//执行
+			}
+			connection.Close();
+
+			string str = System.DateTime.Now.ToString() + "  所有数据已删除！";
+			writeLog(str);
+		}
+
+
+		//对SQLite数据库执行增删改操作，返回受影响的行数。 
+		public static int ExecuteNonQuery(string sql, params SQLiteParameter[] parameters)
+		{
+			int affectedRows = 0;
+			using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+			{
+				using (SQLiteCommand command = new SQLiteCommand(connection))
+				{
+					try
+					{
+						connection.Open();
+						command.CommandText = sql;
+						if (parameters.Length != 0)
+						{
+							command.Parameters.AddRange(parameters);
+						}
+						affectedRows = command.ExecuteNonQuery();
+					}
+					catch (Exception) { throw; }
+				}
+			}
+			return affectedRows;
+		}
+
+
+
+		//执行一个查询语句，返回一个包含查询结果的DataTable
+		public static DataTable ExecuteQuery(string sql, params SQLiteParameter[] parameters)
+		{
+			using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+			{
+				using (SQLiteCommand command = new SQLiteCommand(sql, connection))
+				{
+					if (parameters.Length != 0)
+					{
+						command.Parameters.AddRange(parameters);
+					}
+					SQLiteDataAdapter adapter = new SQLiteDataAdapter(command);
+					DataTable data = new DataTable();
+					try { adapter.Fill(data); }
+					catch (Exception) { throw; }
+					return data;
+				}
+			}
+		}
+
+		public static DataTable ExecuteQuery(string sql)
+		{
+			using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+			{
+				using (SQLiteCommand command = new SQLiteCommand(sql, connection))
+				{
+					SQLiteDataAdapter adapter = new SQLiteDataAdapter(command);
+					DataTable data = new DataTable();
+					try { adapter.Fill(data); }
+					catch (Exception) { throw; }
+					return data;
+				}
+			}
+		}
+
+		//设置默认数据
+		public static void inputData()
+		{
+			using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+			{
+				using (SQLiteCommand command = new SQLiteCommand(connection))
+				{
+					connection.Open();
+					// 插入数据
+					command.CommandText = " INSERT INTO [table1] (ID, Name, clNum, headMaster) VALUES (1,'school_1',3,'wang')";
+					command.ExecuteNonQuery();
+					command.CommandText = " INSERT INTO [table1] (ID, Name, clNum, headMaster) VALUES (2,'school_2',2,'zhang')";
+					command.ExecuteNonQuery();
+					command.CommandText = " INSERT INTO [table1] (ID, Name, clNum, headMaster) VALUES (3,'school_3',1,'li') ";
+					command.ExecuteNonQuery();
+
+					command.CommandText = " INSERT INTO [table2] (ID, Name, belong, headTeacher) VALUES (1,'class_1_1',1,'wang')";
+					command.ExecuteNonQuery();
+					command.CommandText = " INSERT INTO [table2] (ID, Name, belong, headTeacher) VALUES (2,'class_1_2',1,'zhang')";
+					command.ExecuteNonQuery();
+					command.CommandText = " INSERT INTO [table2] (ID, Name, belong, headTeacher) VALUES (3,'class_1_3',1,'li')";
+					command.ExecuteNonQuery();
+					command.CommandText = " INSERT INTO [table2] (ID, Name, belong, headTeacher) VALUES (4,'class_2_1',2,'wang')";
+					command.ExecuteNonQuery();
+					command.CommandText = " INSERT INTO [table2] (ID, Name, belong, headTeacher) VALUES (5,'class_2_2',2,'zhang')";
+					command.ExecuteNonQuery();
+					command.CommandText = " INSERT INTO [table2] (ID, Name, belong, headTeacher) VALUES (6,'class_3_1',3,'li')";
+					command.ExecuteNonQuery();
+
+					command.CommandText = " INSERT INTO [table3] (ID, Name, belong, Tel) VALUES (1,'student_1',1,'wang')";
+					command.ExecuteNonQuery();
+					command.CommandText = " INSERT INTO [table3] (ID, Name, belong, Tel) VALUES (2,'student_2',2,'zhang')";
+					command.ExecuteNonQuery();
+					command.CommandText = " INSERT INTO [table3] (ID, Name, belong, Tel) VALUES (3,'student_3',3,'li')";
+					command.ExecuteNonQuery();
+					command.CommandText = " INSERT INTO [table3] (ID, Name, belong, Tel) VALUES (4,'student_4',4,'wang')";
+					command.ExecuteNonQuery();
+					command.CommandText = " INSERT INTO [table3] (ID, Name, belong, Tel) VALUES (5,'student_5',5,'zhang')";
+					command.ExecuteNonQuery();
+					command.CommandText = " INSERT INTO [table3] (ID, Name, belong, Tel) VALUES (6,'student_6',6,'li')";
+					command.ExecuteNonQuery();
+
+					string str = System.DateTime.Now.ToString() + "  数据导入成功！";
+					writeLog(str);
+				}
+			}
+		}
+	}
+
+}
